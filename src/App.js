6bb59f50cc9fba4d5824e0ed78e23d023a27ec02
vimal <vimalonboard69@gmail.com>
2024-05-15 15:03:17 +0530

    qwe

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
     <div className="App">
       <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
         <ParallaxLayer offset={0} speed={0.25}>
-          <div class="animation_layer parallax" id="artback"></div>
+          <div class="animation_layer parallax" id="artback"> 
+          <center>
+            <div className='dogliens'>Dogliens</div>
+          </center>
+          </div>
         </ParallaxLayer>
+         
         <ParallaxLayer offset={0} speed={0.3}>
           <div class="animation_layer parallax" id="mountain"></div>
         </ParallaxLayer>
